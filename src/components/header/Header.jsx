c79104a5beac8fa25223e303d705b86a4ff73491
Nadir <nadir.bagirov2000@gmail.com>
2025-02-24 01:30:47 +0400

    :sparkles:added popular product

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,63 +1,93 @@
-import { ArrowLeftRight, Menu,  Search, ShoppingBasket } from "lucide-react";
-import heroImage from "../../assets/img/hero.svg";
-import InfoHeader from "./infoHeader/InfoHeader";
-import logo from "../../assets/img/logo.svg";
-import Hcategories from "./hCategories/Hcategories";
+import { ArrowLeftRight, Menu, Search, ShoppingBasket } from 'lucide-react'
+import heroImage from '../../assets/img/hero.svg'
+import InfoHeader from './infoHeader/InfoHeader'
+import logo from '../../assets/img/logo.svg'
+import Hcategories from './hCategories/Hcategories'
 import litleHeaderImg from '../../assets/img/litleHeaderImg.svg'
-import Dropdown from "../miniComponents/select/Select";
+import Dropdown from '../miniComponents/select/Select'
 
 function Header() {
-  return (
-    <header className="flex flex-col  " >
-      <div className="bg-cover bg-no-repeat bg-top  h-[720px] bg-fixed px-28  flex flex-col bg-greyy"
-      style={{ backgroundImage: `url(${heroImage})` }}
-      >
+	return (
+		<header className='flex flex-col  '>
+			<div
+				className='bg-cover bg-no-repeat bg-top  h-[720px] bg-fixed px-28  flex flex-col bg-gray1'
+				style={{ backgroundImage: `url(${heroImage})` }}
+			>
+				<InfoHeader />
 
-        <InfoHeader/>
+				<div className='flex justify-between items-center'>
+					<div>
+						<p className='flex cursor-pointer text-secondary font-medium text-xl leading-5 gap-2 items-center '>
+							<Menu color='white' size={28} />
+							Меню
+						</p>
+					</div>
+					<div>
+						<img src={logo} alt='error' />
+					</div>
+					<div className='flex items-center gap-8'>
+						<ArrowLeftRight color='white' size={28} cursor={'pointer'} />
+						<Search color='white' size={28} cursor={'pointer'} />
+						<ShoppingBasket color='white' size={28} cursor={'pointer'} />
+					</div>
+				</div>
 
-    <div className="flex justify-between items-center">
-            <div>
-                <p className="flex cursor-pointer text-secondary font-medium text-xl leading-5 gap-2 items-center "><Menu color="white" size={28}/>Меню</p>
-            </div>
-        <div>
-            <img src={logo} alt="error" />
-        </div>
-        <div className="flex items-center gap-8">
-        <ArrowLeftRight color="white" size={28} cursor={"pointer"}/>
-        <Search  color="white" size={28} cursor={"pointer"}/>
-        <ShoppingBasket color="white"size={28} cursor={"pointer"} />
-        </div>
-    </div>
+				<Hcategories />
+				<div className='mt-10  w-[600px] flex flex-col gap-10 '>
+					<h1 className='font-extrabold text-6xl leading-20 text-secondary'>
+						Мойки, смесители и аксессуары
+					</h1>
+					<p className='text-secondary text-3xl'>
+						от ведущего производителя сантехники для кухни Reginox
+					</p>
+				</div>
+				<div className='w-full flex justify-end '>
+					<div className='w-[312px] '>
+						<div className='bg-danger flex-1 flex items-center text-secondary text-base font-normal'>
+							<p className='ml-1 relative left-20'>Стальные мойки Reg-color</p>
+							<img src={litleHeaderImg} alt='' />
+						</div>
+					</div>
+				</div>
+			</div>
 
-    <Hcategories/>
-    <div className="mt-10  w-[600px] flex flex-col gap-10 ">
-    <h1 className="font-extrabold text-6xl leading-20 text-secondary">Мойки, смесители
-    и аксессуары</h1>
-    <p className="text-secondary text-3xl">от ведущего производителя сантехники для кухни Reginox</p>
-    </div>
-<div className="w-full flex justify-end ">
-    <div className="w-[312px] ">
-    <div className="bg-danger flex-1 flex items-center text-secondary text-base font-normal">
-      <p className="ml-1 relative left-20">Стальные мойки Reg-color</p>
-      <img src={litleHeaderImg} alt="" />
-    </div>
-</div>
-    </div>  
-    </div>
-
-
-    <div className="flex flex-col gap-20 px-24 h-72 pt-10 bg-greyy" >
-    <h2 className="font-bold text-7xl text-center text-secondary  ">THE CROWN FOR YOUR KITCHEN</h2>
-    <div className="flex justify-between flex-wrap gap-4 relative">
-    <Dropdown number="+65 (0) 67 88 53 63" mail="info@reginox.com.sg" name="Reginox, Singapore" browser="reginox.sg" adress="1 Loyang Lane #03-03 Singapore 508913" />
-    <Dropdown number="+994 (12) 497 36 52" mail="info@azercompany.az" name="Azertexnolayn, Azerbaijan" browser="azertexnolayn.az" adress="Bakı, Qaradağ rayonu, Salyan şosesi 32-ci km" />
-    <Dropdown number="+1 (415) 789-4561" mail="support@usainnovate.com" name="USA Innovate, USA" browser="usainnovate.com" adress="Silicon Valley, 123 Tech Street, CA 94025" />
-    <Dropdown number="+44 (0) 20 7946 0958" mail="hello@brittech.uk" name="BritTech, UK" browser="brittech.uk" adress="London, 25 Oxford Street, W1D 2DN" />
-</div>
-
-    </div>
-    </header>
-  );
+			<div className='flex flex-col gap-20 px-24 h-72 pt-10 bg-gray1'>
+				<h2 className='font-bold text-7xl text-center text-secondary  '>
+					THE CROWN FOR YOUR KITCHEN
+				</h2>
+				<div className='flex justify-between flex-wrap gap-4 relative'>
+					<Dropdown
+						number='+65 (0) 67 88 53 63'
+						mail='info@reginox.com.sg'
+						name='Reginox, Singapore'
+						browser='reginox.sg'
+						adress='1 Loyang Lane #03-03 Singapore 508913'
+					/>
+					<Dropdown
+						number='+994 (12) 497 36 52'
+						mail='info@azercompany.az'
+						name='Azertexnolayn, Azerbaijan'
+						browser='azertexnolayn.az'
+						adress='Bakı, Qaradağ rayonu, Salyan şosesi 32-ci km'
+					/>
+					<Dropdown
+						number='+1 (415) 789-4561'
+						mail='support@usainnovate.com'
+						name='USA Innovate, USA'
+						browser='usainnovate.com'
+						adress='Silicon Valley, 123 Tech Street, CA 94025'
+					/>
+					<Dropdown
+						number='+44 (0) 20 7946 0958'
+						mail='hello@brittech.uk'
+						name='BritTech, UK'
+						browser='brittech.uk'
+						adress='London, 25 Oxford Street, W1D 2DN'
+					/>
+				</div>
+			</div>
+		</header>
+	)
 }
 
-export default Header;
+export default Header
--- src/components/popular/Popular.jsx
+++ src/components/popular/Popular.jsx
@@ -1,21 +1,56 @@
 import amsterdamDark25 from '../../assets/img/popular/amsterdamDark25.svg'
+import YADKIN from '../../assets/img/popular/YADKIN.svg'
+import Diplomat from '../../assets/img/popular/Diplomat.svg'
+import WAFELBLAD from '../../assets/img/popular/WAFELBLAD.svg'
+import Amsterdam34CaffeSilvery from '../../assets/img/popular/Amsterdam34CaffeSilvery.svg'
+import R18370OSPlux from '../../assets/img/popular/R18370OSPlux.svg'
+
+const products = [
+	{
+		id: 1,
+		name: 'Amsterdam 25 Dark chocolate',
+		price: '9 950 руб',
+		image: amsterdamDark25,
+	},
+	{ id: 2, name: 'YADKIN Premium', price: '8 500 руб', image: YADKIN },
+	{ id: 3, name: 'Diplomat Classic', price: '10 200 руб', image: Diplomat },
+	{ id: 4, name: 'WAFELBLAD Original', price: '7 800 руб', image: WAFELBLAD },
+	{
+		id: 5,
+		name: 'Amsterdam 34 Caffe Silvery',
+		price: '11 000 руб',
+		image: Amsterdam34CaffeSilvery,
+	},
+	{ id: 6, name: 'R18370 OSP Lux', price: '12 300 руб', image: R18370OSPlux },
+]
+
 function Popular() {
 	return (
-		<section className='px-28 mt-20 flex flex-col gap-20 '>
-			<h2 className='font-bold text-5xl leading-14 text-center self-start'>
+		<section className='px-28 mt-20 flex flex-col gap-20'>
+			<h2 className='font-bold text-5xl leading-[1.4] text-center self-start'>
 				Популярные товары
 			</h2>
 
-			<div className='grid grid-cols-3 shadow bg-greyy h-72'>
-				<div className=''>
-					<div>
-						<img src={amsterdamDark25} alt='' />
+			<div className='grid grid-cols-3 gap-5 shadow-xl bg-gray-100 p-5'>
+				{products.map(product => (
+					<div
+						key={product.id}
+						className='w-80 mx-5 my-5 flex flex-col hover:-translate-y-5 transition ease-in-out duration-500 group'
+					>
+						<div className='bg-white h-80 flex flex-col items-center justify-between p-4 shadow-lg rounded-lg'>
+							<img
+								src={product.image}
+								alt={product.name}
+								className='h-40 w-auto object-contain'
+							/>
+							<h3 className='text-lg font-semibold'>{product.name}</h3>
+							<p className='text-xl font-bold text-gray-700'>{product.price}</p>
+						</div>
+						<div className='bg-red-500 hover:bg-red-600 duration-500 opacity-0 text-white w-full h-14 flex items-center justify-center cursor-pointer transition group-hover:opacity-100 rounded-lg'>
+							В корзину
+						</div>
 					</div>
-					<div className='absolute'>
-						<h3>Amsterdam 25 Dark chocolate</h3>
-						<p>9 950 руб</p>
-					</div>
-				</div>
+				))}
 			</div>
 		</section>
 	)
